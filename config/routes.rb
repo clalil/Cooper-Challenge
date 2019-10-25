44b3e3fc7a74377061688cb3e62d77ebbc8e1592
clalil <cliljander@gmail.com>
2019-10-25 15:02:51 +0200

    Add DS_Store to gitignore, remove duplicate in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
-    mount_devise_token_auth_for 'User', at: 'auth'
     namespace :v1, defaults: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
       resources :performance_data, only: [:create, :index]
